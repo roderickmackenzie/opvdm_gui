9bb070221a9d2f7a31e26cdb6743405800369103
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2015-01-12 23:18:15 +0000

    added code so a scan can be run from the command line

--- command_args.py
+++ command_args.py
@@ -1,75 +1,28 @@
-#    Organic Photovoltaic Device Model - a drift diffusion base/Shockley-Read-Hall
-#    model for organic solar cells. 
-#    Copyright (C) 2012 Roderick C. I. MacKenzie
-#
-#	roderick.mackenzie@nottingham.ac.uk
-#	www.opvdm.com
-#	Room B86 Coates, University Park, Nottingham, NG7 2RD, UK
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-
 import sys
-import pdb
-import pygtk
-pygtk.require('2.0')
-import gtk
-
 import os
 import shutil
-from scan import scan_class
-from tab import tab_class
-from search import find_fit_error
-from optics import class_optical
-import signal
-import subprocess
-from inp import inp_update_token_value
-from inp import inp_get_token_value
-from inp import inp_isfile
-from util import set_exe_command
-from util import get_orig_inp_file_path
 from util import opvdm_clone
 from export_as import export_as
-from emesh import tab_electrical_mesh
-from welcome import welcome_class
-from copying import copying
-from hpc import hpc_class
-from tab_homo import tab_bands
-from tempfile import mkstemp
-from plot_gen import plot_gen
-from plot_gen import set_plot_auto_close
 from import_archive import import_archive
-from about import about_dialog_show
 from util import find_data_file
-from notice import notice
-import os, fnmatch
-import pyinotify
-import pynotify
-import threading
-import time
-import gobject
-import numpy
+from util import opvdm_copy_src
+import fnmatch
 import logging
 import time
-from util import check_is_config_file
-from window_list import windows
-from config import config
-from plot import plot_data
 from import_archive import delete_scan_dirs
-from undo import undo_list_class
+from import_archive import clean_scan_dirs
 from ver import ver
+from import_archive import import_scan_dirs
+from udp_server import udp_server
+from udp_client import udp_client
+from make_man import make_man
+from scan_util import tree_load_program
+from scan_util import tree_gen
+from scan_item import scan_item_load
+from scan_item import scan_items_index_item
+from server import server
+from util import set_exe_command
+import gtk 
 def command_args(argc,argv):
 	if argc>=2:
 		if argv[1]=="--help":
@@ -83,8 +36,10 @@ def command_args(argc,argv):
 			print "\t\t\tusage --import abc.gz ./path/to/output/ "
 			print "\t--patch\t\tpatch a simulation with an archived simulation"
 			print "\t--clone\t\tgenerate a clean simulation in the current directory"
-			print "\t--clean\t\tcleans the current simulation directory"
+			print "\t--clean\t\tcleans the current simulation directory deleting .dat files and scan dirs"
 			print "\t--dump-tab (output file)\t\tdumps simulation parameters as jpg"
+			print "\t--import-scandirs\t\tOnly imports the scan directories"
+			print "\t--clean-scandirs\t\tDeletes the content of all scan dirs"
 
 			print ""
 			print "Additional information about opvdm is available at http://www.opvdm.com."
@@ -94,6 +49,9 @@ def command_args(argc,argv):
 		if argv[1]=="--version":
 			print ver()
 			sys.exit(0)
+		if argv[1]=="--import-scandirs":
+			import_scan_dirs("./",argv[2])
+			exit(0)
 		if argv[1]=="--export":
 			export_as(argv[2])
 			sys.exit(0)
@@ -109,12 +67,19 @@ def command_args(argc,argv):
 		if argv[1]=="--clone":
 			opvdm_clone()
 			sys.exit(0)
+		if argv[1]=="--clone-src":
+			opvdm_copy_src(argv[2])
+			sys.exit(0)
+
 		if argv[1]=="--file_info":
 			data=plot_data()
 			data.dump_file()
 			sys.exit(0)
 		if argv[1]=="--clean":
 			delete_scan_dirs("./")
+			if os.path.isdir("./pub"):
+				print "Deleteing pub"
+				shutil.rmtree("./pub")
 			files = os.listdir("./")
 			for file in files:
 				remove=False
@@ -138,3 +103,38 @@ def command_args(argc,argv):
 					print file
 					os.remove(file)
 			sys.exit(0)
+		if argv[1]=="--clean-scandirs":
+			clean_scan_dirs(os.getcwd())
+			sys.exit(0)
+
+		if argv[1]=="--server":
+			obj=udp_server()
+			obj.start()
+
+		if argv[1]=="--client":
+			client=udp_client()
+			client.init()
+
+		if argv[1]=="--make-man":
+			make_man()
+			sys.exit(0)
+
+		if argv[1]=="--run-scan":
+			program_list=[]
+			exe_command , exe_name  =  set_exe_command()
+			print scan_items_index_item("imps/Modulation frequency")
+			scan_item_load("./suns/scan_items.inp")
+			tree_load_program(program_list,"./suns/")
+			print program_list
+			watch_dir=os.path.join(os.getcwd())+"/suns/"
+			commands=tree_gen(program_list,os.getcwd(),os.path.join(os.getcwd(),"suns"))
+			myserver=server()
+			myserver.init(watch_dir)
+			myserver.clear_cache()
+			for i in range(0, len(commands)):
+				myserver.add_job(commands[i])
+				print "Adding job"+commands[i]
+			myserver.simple_run(exe_command)
+
+			sys.exit(0)
+
