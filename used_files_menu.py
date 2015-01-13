490776b96178d94a20e79692b49b5093b2d0bf65
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2015-01-13 20:17:58 +0000

    taken code out of used_files_menu

--- used_files_menu.py
+++ used_files_menu.py
@@ -1,25 +1,3 @@
-#    Organic Photovoltaic Device Model - a drift diffusion base/Shockley-Read-Hall
-#    model for organic solar cells. 
-#    Copyright (C) 2012 Roderick C. I. MacKenzie
-#
-#	roderick.mackenzie@nottingham.ac.uk
-#	www.opvdm.com
-#	Room B86 Coates, University Park, Nottingham, NG7 2RD, UK
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 import pygtk
 pygtk.require('2.0')
 import gtk
@@ -30,90 +8,46 @@ import copy
 import pdb
 import logging
 from plot_command import plot_command_class
+import glob
+from inp import inp_search_token_value
+from inp import inp_load_file
 
 class used_files_menu:
 	def __init__(self):
 		self.menu=gtk.Menu()
 
-	def init(self,config_file,callback):
-		logging.info("used_file_menu init")
+	def refresh(self):
 		try:
 			for i in self.menu.get_children():
 				self.menu.remove(i)
 		except:
 			pass
 
-		self.last_items=[]
-		self.config_file=config_file
+		self.list=[]
+		files=[]
+		for my_file in glob.glob(os.path.join(self.base_dir,"*.oplot")):
+			files.append(my_file)
+
+		sorted(files)
+		for i in range(0,len(files)):
+			self.append(files[i])
+
+	def init(self,search_path,callback):
+
+		self.base_dir=search_path
 		self.callback=callback
-		self.base_dir=os.path.dirname(config_file)+'/'
-	
-	def plot_token_id_gen(self,plot_token):
-		return plot_token.file0+plot_token.tag0+plot_token.file1+plot_token.tag1
 
-	def plot_token_check_list(self,items,plot_token_id):
-		for i in range(0,len(items)):
-			check=self.plot_token_id_gen(items[i])
-			if check==plot_token_id:
-				return True
-		return False
+		self.refresh()
 
-	def append(self,plot_token,save):
-		logging.info("used_file_menu append")
-		file_name=self.plot_token_id_gen(plot_token)
-		
-		if self.plot_token_check_list(self.last_items,file_name)==False:
-			menu_item = gtk.MenuItem(file_name)		   
+	def append(self,file_name):
+		lines=[]
+		plot_token=plot_command_class()
+		if plot_load_token(plot_token,file_name)==True:
+			menu_item = gtk.MenuItem(os.path.basename(file_name).split(".")[0])		   
 			self.menu.append(menu_item)
-			self.last_items.append(plot_token)
-			ref=self.last_items[len(self.last_items)-1]
-			menu_item.connect("activate", self.callback,ref)
-			if save==True:
-				self.save_list()
+			self.list.append(plot_token)
+			menu_item.connect("activate", self.callback,self.list[len(self.list)-1])
 			menu_item.show()
-
-	def save_list(self):
-		a = open(self.config_file, "w")
-		a.write(str(len(self.last_items))+"\n")
-		for i in range(0,len(self.last_items)):
-			a.write(self.last_items[i].path+"\n")
-			a.write(self.last_items[i].file0+"\n")
-			a.write(self.last_items[i].tag0+"\n")
-			a.write(self.last_items[i].file1+"\n")
-			a.write(self.last_items[i].tag1+"\n")
-		
-		a.close()
-
-	def reload_list(self):
-		logging.info("used_file_menu reload_list")
-		print "Loading menu",self.config_file
-
-		for i in self.menu.get_children():
-			self.menu.remove(i)
-		try:
-			f=open(self.config_file)
-			config = f.readlines()
-			f.close()
-
-			for ii in range(0, len(config)):
-				config[ii]=config[ii].rstrip()
-
-			number=int(config[0])
-			for ii in range(0, number):
-				pos=1+ii*5
-
-				readitem=plot_command_class()
-				readitem.path=config[pos+0]
-				readitem.file0=config[pos+1]
-				readitem.tag0=config[pos+2]
-				readitem.file1=config[pos+3]
-				readitem.tag1=config[pos+4]
-
-				if readitem.path.startswith(self.base_dir)==True:
-					self.append(readitem,False)
-
-		except IOError:
-			print "No file found"
 
 
 
