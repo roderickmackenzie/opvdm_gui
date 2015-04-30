a72013e904792ee460fecfa48299eae0caf39278
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2015-04-30 15:39:51 +0100

    windows welcome screen update

--- welcome.py
+++ welcome.py
@@ -7,9 +7,8 @@
 #	Room B86 Coates, University Park, Nottingham, NG7 2RD, UK
 #
 #    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU General Public License v2.0, as published by
+#    the Free Software Foundation.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,7 +26,7 @@ import sys
 import os
 import shutil
 import commands
-import webkit
+#import webkit
 
 class welcome_class(gtk.HBox):
 	
@@ -42,31 +41,31 @@ class welcome_class(gtk.HBox):
 		gtk.main_quit()
 		return False
 
-	def on_active(self, widge, data=None):
+	#def on_active(self, widge, data=None):
 		'''When the user enters an address in the bar, we check to make
 		   sure they added the http://, if not we add it for them.  Once
 		   the url is correct, we just ask webkit to open that site.'''
-		url = self.url_bar.get_text()
-		try:
-		    url.index("://")
-		except:
-		    url = "http://"+url
-		self.url_bar.set_text(url)
-		self.web.open(url)
+		#url = self.url_bar.get_text()
+		#try:
+		#    url.index("://")
+		#except:
+		#    url = "http://"+url
+		#self.url_bar.set_text(url)
+		#self.web.open(url)
 
-	def go_back(self, widget, data=None):
-		self.web.go_back()
+	#def go_back(self, widget, data=None):
+		#self.web.go_back()
 
-	def go_forward(self, widget, data=None):
-		self.web.go_forward()
+	#def go_forward(self, widget, data=None):
+	#	self.web.go_forward()
 
-	def refresh(self, widget, data=None):
-		self.web.reload()
+	#def refresh(self, widget, data=None):
+	#	self.web.reload()
 
-	def update_buttons(self, widget, data=None):
-		self.url_bar.set_text( widget.get_main_frame().get_uri() )
-		self.back_button.set_sensitive(self.web.can_go_back())
-		self.forward_button.set_sensitive(self.web.can_go_forward())
+	#def update_buttons(self, widget, data=None):
+	#	self.url_bar.set_text( widget.get_main_frame().get_uri() )
+	#	self.back_button.set_sensitive(self.web.can_go_back())
+	#	self.forward_button.set_sensitive(self.web.can_go_forward())
 
 
 	def wow(self,image_name):
@@ -87,29 +86,29 @@ class welcome_class(gtk.HBox):
 		#		+output+"</big>")
 
 
-		self.web = webkit.WebView() 
+		#self.web = webkit.WebView() 
 
 		sw = gtk.ScrolledWindow() 
-		sw.add(self.web) 
+		#sw.add(self.web) 
 
 
 		toolbar = gtk.Toolbar()
 
 		self.back_button = gtk.ToolButton(gtk.STOCK_GO_BACK)
-		self.back_button.connect("clicked", self.go_back)
+		#self.back_button.connect("clicked", self.go_back)
 
 		self.forward_button = gtk.ToolButton(gtk.STOCK_GO_FORWARD)
-		self.forward_button.connect("clicked", self.go_forward)
+		#self.forward_button.connect("clicked", self.go_forward)
 
 		refresh_button = gtk.ToolButton(gtk.STOCK_REFRESH)
-		refresh_button.connect("clicked", self.refresh)
+		#refresh_button.connect("clicked", self.refresh)
 
 		toolbar.add(self.back_button)
 		toolbar.add(self.forward_button)
 		toolbar.add(refresh_button)
 
 		self.url_bar = gtk.Entry()
-		self.url_bar.connect("activate", self.on_active)
+		#self.url_bar.connect("activate", self.on_active)
 
 		box=gtk.HBox(True, 1)
 		box.set_size_request(500,-1)
@@ -120,7 +119,7 @@ class welcome_class(gtk.HBox):
 		url_tb_item.show()
 		toolbar.add(url_tb_item)
 
-		self.web.connect("load_committed", self.update_buttons)
+		#self.web.connect("load_committed", self.update_buttons)
 
 
 		vbox = gtk.VBox(False, 0)
@@ -131,7 +130,7 @@ class welcome_class(gtk.HBox):
 		self.show_all()
 
 
-		self.web.open("http://www.roderickmackenzie.eu/opvdm/welcome.html")
+		#self.web.open("http://www.roderickmackenzie.eu/opvdm/welcome.html")
 		self.add(vbox)
 
 		#self.add(label)
